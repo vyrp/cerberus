5755eafe2d8ff7fb960338403ec8506a09734d8e
Marcelo Sobral <marceloflorenciosobral@gmail.com>
2013-07-03 04:08:51 -0300

    Android layout

--- Android/src/br/itabits/cerberus/LoginActivity.java
+++ Android/src/br/itabits/cerberus/LoginActivity.java
@@ -2,7 +2,11 @@ package br.itabits.cerberus;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -10,6 +14,16 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        Button loginButton = (Button) findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				 Intent openBorrow = new Intent(LoginActivity.this,BorrowActivity.class);
+				 startActivity(openBorrow);
+				 finish();
+			}
+		});
     }
 
     @Override
